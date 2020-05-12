0536d182f6ee08489f62d533b1e85a3e621ad06e
Nima Nassehi <appdevelopment@utt.qc.ca>
2020-05-12 10:56:43 -0400

    Initial Commit

---
+++ Uber Clone Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Uber Clone Tutorial
+//
+//  Created by Nima on 2020-05-12.
+//  Copyright © 2020 Azrieli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
